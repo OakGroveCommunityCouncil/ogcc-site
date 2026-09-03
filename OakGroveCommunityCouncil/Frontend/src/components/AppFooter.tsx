87948513bbe4c7bfad31c1b627d73cb9a7530ce4
Ian Andrewson <ianandrewson@gmail.com>
2026-09-02 18:08:56 -0700

    scaffolded root, added skeleton header and footer, added theme.

---
+++ OakGroveCommunityCouncil/Frontend/src/components/AppFooter.tsx
@@ -0,0 +1,7 @@
+export const AppFooter = () => {
+  return (
+    <>
+      <div>This is our footer</div>
+    </>
+  )
+}
--- OakGroveCommunityCouncil/Frontend/src/routes/__root.tsx
+++ OakGroveCommunityCouncil/Frontend/src/routes/__root.tsx
@@ -4,15 +4,24 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import '../styles.css'
+import { AppHeader } from '#/components/AppHeader'
+import { AppFooter } from '#/components/AppFooter'
+import { ThemeProvider, useTheme } from '@mui/material'
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
+  const theme = useTheme()
+
   return (
     <>
-      <Outlet />
+      <ThemeProvider theme={theme}>
+        <AppHeader />
+        <Outlet />
+        <AppFooter />
+      </ThemeProvider>
       <TanStackDevtools
         config={{
           position: 'bottom-right',
