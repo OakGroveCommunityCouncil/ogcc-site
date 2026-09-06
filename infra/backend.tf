ec7b72bb6cf9ebd783ee58a785e685303efe1d82
Daniel <3745227+ddgcodes@users.noreply.github.com>
2026-09-05 17:32:52 -0700

    Comment clarification
    Noting which environment variable is mapped to which backend
    configuration value.

--- infra/backend.tf
+++ infra/backend.tf
@@ -5,8 +5,8 @@ terraform {
     use_oidc         = true
     use_azuread_auth = true
     # These values are read from environment variables in GitHub Actions (Terraform variables can't be used here)
-    # tenant_id
-    # client_id
+    # tenant_id -> ARM_TENANT_ID
+    # client_id -> ARM_CLIENT_ID
     # These values must be passed when invoking the Terraform CLI in the Workflow
     # storage_account_name
     # container_name 
